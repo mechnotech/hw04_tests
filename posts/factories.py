e0b75d0e5adc600fac3def7baad0dec43916f013
Evgenii Shumilov <mechnotech@yandex.ru>
2020-05-21 12:30:29 +0300

    last try 0.1.2 factories - ok

--- posts/factories.py
+++ posts/factories.py
@@ -5,8 +5,8 @@ import factory.random
 from .models import User, Post
 
 
-def reset_factory_random(salt='3ef42b5001a'):
-    factory.random.reseed_random(f'Yatube project seed {salt}')
+def reset_factory_random(seed='3ef42b5001a'):
+    factory.random.reseed_random(f'Yatube project seed {seed}')
 
 
 class UserFactory(factory.DjangoModelFactory):
